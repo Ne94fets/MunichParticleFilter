24caf37c14d3a0c081dfd3b27304b17f4402115f
Steffen Kastner <kastner_s@freenet.de>
2023-02-21 14:10:28 +0100

    simulate ball trajectory and update particle filter

--- munich.js
+++ munich.js
@@ -1,7 +1,107 @@
+// ######################
+// # BEHAVIOR
+// ######################
+class BallStateInit extends AInitializer {
+	#bboxMin; #bboxMax;
+	constructor(bboxMin, bboxMax) {
+		super();
+		this.bboxMin = bboxMin;
+		this.bboxMax = bboxMax;
+	}
+	
+	init(particles) {
+		for(let particle of particles) {
+			let x = this.bboxMin[0] + Math.random() * (this.bboxMax[0] - this.bboxMin[0]);
+			let y = this.bboxMin[1] + Math.random() * (this.bboxMax[1] - this.bboxMin[1]);
+			particle.state.pos = math.matrix([x, y]);
+			particle.weight = 1/particles.length;
+		}
+	}
+}
+class BallTransition extends ATransition {
+	transition(particles, control) { 
+		const dt = control.getDt();
+		for (let particle of particles) {
+			particle.pos = particle.pos + particle.velocity * dt;
+		}
+	}
+}
+class BallEvaluation extends AEvaluation {
+	evaluate(particles, observations) { 
+		// evaluate each particle
+		for (let particle of particles) {
+			
+			// calculate probability of this state given all observations
+			for (let obs of observations.getObservations()) {
+				const d = particle.pos - obs
+			}
+		}
+	}
+}
+
+
+// ######################
+// # MODELS
+// ######################
+class BallState extends AParticleState {
+	pos = math.matrix([0, 0]);
+	velocity = math.matrix([0, 0]);
+	
+	multiplyWith(factor) {
+		this.pos = math.multiply(this.pos, factor);
+		this.velocity = math.multiply(this.velocity, factor);
+	}
+	addTo(dstState) {
+		dstState.pos = math.add(this.pos, dstState.pos);
+		dstState.velocity = math.add(this.velocity, dstState.velocity);
+	}
+	clone() {
+		let result = new BallState();
+		this.addTo(result);
+		return result;
+	}
+}
+class BallControl extends AControl {
+	#dt;
+	
+	constructor(dt) {
+		super();
+		this.#dt = dt;
+	}
+	
+	getDt() {
+		return this.#dt;
+	}
+	
+}
+class BallObservations extends AObservations {
+	#observations = [];
+	
+	constructor(observations) {
+		super();
+		this.#observations = observations;
+	}
+	
+	getObservations() {
+		return this.#observations;
+	}
+}
+
+
+// ######################
+// # SIMULATION
+// ######################
 class Simulation {
-	constructor() {
+	#particleFilter = null;
+	#pfInitializer = null;
+	
+	constructor(particleFilter, pfInitializer) {
+		this.#particleFilter = particleFilter;
+		this.#pfInitializer = pfInitializer;
+		
 		this.inputs = {};
 		this.parameters = {};
+		this.#addInput("simulationSpeed");
 		this.#addInput("simulationDuration");
 		this.#addInput("simulationDelta");
 		this.#addInput("gravity");
@@ -9,7 +109,7 @@ class Simulation {
 		this.#addInput("launchPositionY");
 		this.#addInput("launchVelocity");
 		this.#addInput("launchAngle");
-		this.#replot();
+		this.#simulate();
 	}
 	
 	#addInput(name) {
@@ -19,11 +119,12 @@ class Simulation {
 		el.addEventListener('change', (event) => {
 			const value = parseFloat(event.target.value);
 			this.parameters[name] = value;
-			this.#replot();
+			this.#simulate();
 		});
 	}
 	
-	#replot() {
+	#simulate() {
+		const sleep = 1 / this.parameters["simulationSpeed"];
 		const duration = this.parameters["simulationDuration"];
 		const dt = this.parameters["simulationDelta"];
 		const g = this.parameters["gravity"];
@@ -35,27 +136,55 @@ class Simulation {
 		const v0x = v0 * Math.cos(angle);
 		const v0y = v0 * Math.sin(angle);
 		
+		// init particleFilter
+		this.#particleFilter.initWith(this.#pfInitializer);
+		
 		// simulate perfect ball curve
 		this.timestamps = [];
 		this.gtCurveX = [];
 		this.gtCurveY = [];
-		for (let t = 0; t < duration; t += dt) {
+		var t = 0;
+		const intervalHandler = setInterval(() => {
 			this.timestamps.push(t);
 			
 			const x = x0 + v0x * t;
 			const y = y0 + v0y * t + (1.0/2.0) * g * t*t;
 			this.gtCurveX.push(x);
 			this.gtCurveY.push(y);
-		}
+			
+			const control = new BallControl(dt);
+			const observations = new BallObservations([math.matrix([x, y])]);
+			
+			this.#particleFilter.update(control, observations);
 		
-		Plotly.newPlot("gd", /* JSON object */ {
-			"data": [{ 
-				"x": this.gtCurveX,
-				"y": this.gtCurveY,
-				
-			}],
-		});
+			// plot what has happend this step
+			Plotly.newPlot("gd", {
+				"data": [{ 
+					"x": this.gtCurveX,
+					"y": this.gtCurveY,
+					
+				}],
+			});
+			
+			// check if simulation is done
+			if (t > duration) {
+				clearInterval(intervalHandler);
+			}
+			
+			// increment time
+			t += dt;
+		}, sleep * 1000);
 	}
 }
 
-const simulation = new Simulation();
+
+
+// APP
+let transition = new BallTransition();
+let evaluation = new BallEvaluation();
+let estimation = new EstimationWeightedAverage();
+let resampling = new ResamplingSimple();
+let pf = new ParticleFilter(5000, BallState, transition, evaluation, estimation, resampling);
+
+let ballInitializer = new BallStateInit([-5, -5], [5, 5]);
+const simulation = new Simulation(pf, ballInitializer);
